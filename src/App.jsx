fb929cce9fdf11281965af029e8a8615d596a5f7
PerfildeSara <saradesarrolladora@gmail.com>
2023-09-19 15:35:04 -0500

    React Project on Github Pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import Header from './components/Header' 
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Riddle from './pages/Riddle'
-import AboutMe from './pages/AboutMe'
-import Experience from './pages/Experience'
-import ContactMe from './pages/ContactMe'
-import Projects from './pages/Projects'
-import Home from './pages/Home'
-import Footer from './components/Footer'
-import { routes } from './Routes'
+import { useState } from 'react';
+import Header from './components/Header' ;
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Riddle from './pages/Riddle';
+import AboutMe from './pages/AboutMe';
+import Experience from './pages/Experience';
+import ContactMe from './pages/ContactMe';
+import Projects from './pages/Projects';
+import Home from './pages/Home';
+import Footer from './components/Footer';
+import { routes } from './Routes';
 
 
 function App() {
